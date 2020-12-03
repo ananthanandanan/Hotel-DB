964275d25f42caae7e2e442ac4b917b7ec9d65be
ananthanandanan <ananthanandanan@gmail.com>
2020-12-03 22:52:26 +0530

    Dashboard: add initial Ui for dashboard

---
+++ src/dashboard/dashboardController.java
@@ -0,0 +1,11 @@
+package dashboard;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+public class dashboardController {
+    public BorderPane boardpane;
+    public AnchorPane anchor;
+    public HBox hbtitle;
+}
