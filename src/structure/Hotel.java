91a25487a5853127c3e1a1eefe22261eb5f8479d
ananthanandanan <ananthanandanan@gmail.com>
2020-12-03 10:42:31 +0530

    add package structure - Add class Hotel with fields

---
+++ src/structure/Hotel.java
@@ -0,0 +1,45 @@
+package structure;
+
+
+public class Hotel {
+
+    String hotel_name ;
+    float revenue_planned;
+    String state;
+    String date;
+
+    public  Hotel(String hotel_name, float revenue_planned, String state, String date){
+        this.hotel_name = hotel_name;
+        this.revenue_planned = revenue_planned;
+        this.state = state;
+        this.date = date;
+
+    }
+    //Getters
+    public  String getHotel_name(){
+        return  hotel_name;
+    }
+    public  float getRevenue_planned(){
+        return  revenue_planned;
+    }
+    public  String getState(){
+        return  state;
+    }
+    public  String getDate(){
+        return  date;
+    }
+    //Setters
+    public void setHotel_name(String hotel_name) {
+        this.hotel_name = hotel_name;;
+    }
+    public void setRevenue_planned(float revenue_planned) {
+       this.revenue_planned = revenue_planned;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
