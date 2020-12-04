c0b21bb611ef8d1ccf201f6d7aaa0e74e94b74f6
ananthanandanan <ananthanandanan@gmail.com>
2020-12-04 23:55:21 +0530

    Login: add login package with loaders

--- src/login/Main.java
+++ src/login/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Hotel Management System");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
@@ -21,3 +22,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
