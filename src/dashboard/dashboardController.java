c6c4e605d19420e9fb09e092eb97d9715e69d1a4
ananthanandanan <ananthanandanan@gmail.com>
2020-12-04 16:20:56 +0530

    Dashboard: intermediate ui

--- src/dashboard/dashboardController.java
+++ src/dashboard/dashboardController.java
@@ -1,11 +1,13 @@
 package dashboard;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class dashboardController {
     public BorderPane boardpane;
-    public AnchorPane anchor;
-    public HBox hbtitle;
+    public Label rectitle;
+    public VBox vpane;
 }
