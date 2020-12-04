85fdb6004a576fbe1e15b5e1d1a27f5e5970b714
ananthanandanan <ananthanandanan@gmail.com>
2020-12-04 23:36:02 +0530

    Dashboard: dashboard design completed + add css file

--- src/dashboard/dashboardController.java
+++ src/dashboard/dashboardController.java
@@ -1,13 +1,22 @@
 package dashboard;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class dashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class dashboardController implements Initializable {
     public BorderPane boardpane;
-    public Label rectitle;
     public VBox vpane;
+    public Label rectitle;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
