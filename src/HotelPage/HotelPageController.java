d05a0aa8170a265fc5f333ebd186e185e3e5c8ad
ananthanandanan <ananthanandanan@gmail.com>
2020-12-08 21:03:30 +0530

    Hotelpage: fetch from postgresdb mechanism

--- src/HotelPage/HotelPageController.java
+++ src/HotelPage/HotelPageController.java
@@ -1,19 +1,31 @@
 package HotelPage;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-
+import structure.Hotel;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
+import  database.DatabaseConnector;
 
 public class HotelPageController implements Initializable {
-
+    @FXML
+    private TableView<Hotel> hotelTable;
+    @FXML
+    private TableColumn<Hotel, String> C1;
+    @FXML
+    private TableColumn<Hotel, Float> C2;
+    @FXML
+    private TableColumn<Hotel, String> C3;
+    @FXML
+    private TableColumn<Hotel, String> C4;
     @FXML
     private Hyperlink backlabel;
     @FXML
@@ -32,9 +44,41 @@ public class HotelPageController implements Initializable {
     private Button menuButton;
     @FXML
     private Button EmployeeButton;
+    private PreparedStatement prep_stmt = null;
+    private ResultSet re = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        C1.setCellValueFactory(new PropertyValueFactory<Hotel, String>("hotel_name"));
+        C2.setCellValueFactory(new PropertyValueFactory<Hotel, Float>("revenue_planned"));
+        C3.setCellValueFactory(new PropertyValueFactory<Hotel, String>("state"));
+        C4.setCellValueFactory(new PropertyValueFactory<Hotel, String>("date"));
+
+
+        final ObservableList<Hotel> hotellist = FXCollections.observableArrayList();
+
+        try {
+            Connection connection = DatabaseConnector.getConnnection();
+            connection.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+            //get the data from db
+            prep_stmt = connection.prepareStatement("SELECT * FROM Hotel");
+             re = prep_stmt.executeQuery();
+            while (re.next()){
+                hotellist.add(new Hotel(re.getString("Hotel_name"),re.getFloat("Revenue_planned"), re.getString("State"),
+                        re.getString("Start_date")));
+            }
+            re.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+        hotelTable.setItems(hotellist);
 
     }
     @FXML
