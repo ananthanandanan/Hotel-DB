e045753cae8cfdc77c961ae0dabddc6ef00b8d3b
ananthanandanan <ananthanandanan@gmail.com>
2020-12-08 22:56:53 +0530

    Database conn: add menu chefs

--- src/structure/employee.java
+++ src/structure/employee.java
@@ -1,55 +1,77 @@
 package structure;
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.Date;
 
 public class employee {
-    private String SSN;
-    private String name;
-    private Date doj;
-    private String designation;
+    SimpleStringProperty SSN;
+    SimpleStringProperty name;
+    SimpleStringProperty doj;
+    SimpleStringProperty designation;
 
-    public employee(String SSN,String name,Date doj,String designation)
+    public employee(String SSN,String name,String doj,String designation)
     {
-        this.SSN=SSN;
-        this.name=name;
-        this.doj=doj;
-        this.designation=designation;
+        this.SSN= new SimpleStringProperty(SSN);
+        this.name= new SimpleStringProperty(name);
+        this.doj= new SimpleStringProperty(doj);
+        this.designation= new SimpleStringProperty(designation);
+    }
+
+    public employee(String SSN,String name,String doj)
+    {
+        this.SSN= new SimpleStringProperty(SSN);
+        this.name= new SimpleStringProperty(name);
+        this.doj= new SimpleStringProperty(doj);
     }
 
     public employee(){}
 
-
-    //getters
-
-    public String get_ssn() {return SSN;}
-    public String get_name() {return name;}
-    public Date get_doj(){return doj;}
-    public String get_designation(){return designation;}
-
-    //setters
-
-    public void set_ssn(String SSN)
-    {
-        this.SSN=SSN;
+    public String getSSN() {
+        return SSN.get();
     }
 
-    public void set_name(String name)
-    {
-        this.name=name;
+    public SimpleStringProperty SSNProperty() {
+        return SSN;
     }
 
-    public void set_doj(Date doj)
-    {
-        this.doj=doj;
+    public void setSSN(String SSN) {
+        this.SSN.set(SSN);
     }
 
-    public void set_designation(String designation)
-    {
-        this.designation=designation;
+    public String getName() {
+        return name.get();
     }
 
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name.set(name);
+    }
 
+    public String getDoj() {
+        return doj.get();
+    }
 
+    public SimpleStringProperty dojProperty() {
+        return doj;
+    }
 
+    public void setDoj(String doj) {
+        this.doj.set(doj);
+    }
+
+    public String getDesignation() {
+        return designation.get();
+    }
+
+    public SimpleStringProperty designationProperty() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation.set(designation);
+    }
 }
