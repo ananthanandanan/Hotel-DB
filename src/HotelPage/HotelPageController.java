a130ac05721fcfd1be74674cfc2eda1838c2fbea
ronjacobvarghese <generalsmoke23@gmail.com>
2020-12-09 01:06:48 +0530

    Improved DashBoard UI

--- src/HotelPage/HotelPageController.java
+++ src/HotelPage/HotelPageController.java
@@ -1,16 +1,22 @@
 package HotelPage;
 
+import Transports.Paths;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import structure.Hotel;
 import java.net.URL;
 import java.sql.*;
@@ -87,23 +93,103 @@ public class HotelPageController implements Initializable {
     }
     @FXML
     public void goToHotel(ActionEvent actionEvent) {
-        new dashboard.dashboardController().goToHotel(actionEvent);
+        System.out.println("Check in button clicked");
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(Paths.HOTELPAGEVIEW));
+            Stage window= (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setTitle("Hotel page");
+            Scene scene = new Scene(root);
+            window.setScene(scene);
+            window.show();
+
+            root.requestFocus();
+        }
+        catch (Exception ex) {
+            System.out.println("Error load Checkin FXML !");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     @FXML
     public void goToChefs(ActionEvent actionEvent) {
-        new dashboard.dashboardController().goToChefs(actionEvent);
+        System.out.println("Check in button clicked");
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(Paths.CHEFSPAGEVIEW));
+            Stage window= (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setTitle("Chefs page");
+            Scene scene = new Scene(root);
+            window.setScene(scene);
+            window.show();
+
+            root.requestFocus();
+        }
+        catch (Exception ex) {
+            System.out.println("Error load Checkin FXML !");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     @FXML
     public void goToBranch(ActionEvent actionEvent) {
-        new dashboard.dashboardController().goToBranch(actionEvent);
+        System.out.println("Check in button clicked");
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(Paths.BRANCHPAGEVIEW));
+            Stage window= (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setTitle("Branch page");
+            Scene scene = new Scene(root);
+            window.setScene(scene);
+            window.show();
+
+            root.requestFocus();
+        }
+        catch (Exception ex) {
+            System.out.println("Error load Checkin FXML !");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     @FXML
     public void gotToMenu(ActionEvent actionEvent) {
-        new dashboard.dashboardController().gotToMenu(actionEvent);
+        System.out.println("Check in button clicked");
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(Paths.MENUPAGEVIEW));
+            Stage window= (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setTitle("Menu page");
+            Scene scene = new Scene(root);
+            window.setScene(scene);
+            window.show();
+
+            root.requestFocus();
+        }
+        catch (Exception ex) {
+            System.out.println("Error load Checkin FXML !");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     @FXML
     public void gotToEmployee(ActionEvent actionEvent) {
-        new dashboard.dashboardController().gotToEmployee(actionEvent);
+        System.out.println("Check in button clicked");
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(Paths.EMPLOYEEPAGEVIEW));
+            Stage window= (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setTitle(" Employee page");
+            Scene scene = new Scene(root);
+            window.setScene(scene);
+            window.show();
+
+            root.requestFocus();
+        }
+        catch (Exception ex) {
+            System.out.println("Error load Checkin FXML !");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     @FXML
     public void gotToHome(ActionEvent actionEvent) {
@@ -111,4 +197,5 @@ public class HotelPageController implements Initializable {
     }
     @FXML
     public void gotoLogin(MouseEvent actionEvent){new dashboard.dashboardController().gotoLogin(actionEvent);}
+
 }
